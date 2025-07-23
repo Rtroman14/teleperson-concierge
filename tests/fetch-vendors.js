c5c6860757b1811648c5d958bab1e8c608732d37
Ryan Roman <rtroman14@gmail.com>
2025-07-23 08:56:40 -0600

    migrate to new apis

--- tests/fetch-vendors.js
+++ tests/fetch-vendors.js
@@ -1,19 +1,33 @@
-import TelepersonAPIs from "../lib/teleperson-apis.js";
+import TelepersonAPIs from "../lib/teleperson-apis.new.js";
+
+// const email = "jesse@teleperson.com";
+const email = "ryan@webagent.ai";
 
 (async () => {
     try {
         try {
-            // const user = await TelepersonAPIs.fetchUserByEmail("ryan@webagent.ai");
+            const vendors = await TelepersonAPIs.fetchTopVendors({
+                email,
+            });
 
-            // console.log(`user -->`, user);
+            const data = {
+                user: null,
+                numVendors: vendors.data.data.length,
+                vendorNames: vendors.data.data.map((vendor) => vendor.companyName),
+                vendorDetails: vendors.data.data.map((vendor) => ({
+                    name: vendor.companyName,
+                    description: vendor.companyOverview,
+                    // websiteURL: vendor.websiteURL,
+                    industry: vendor.industry,
+                })),
+            };
 
-            // const vendors = await TelepersonAPIs.fetchVendorsByUserId(3);
-            const vendors = await TelepersonAPIs.fetchVendorsByUserId(6545);
-            // console.log(`vendors -->`, vendors);
+            const vendorsOld = await TelepersonAPIs.userAndVendors(email);
+            console.log(`vendorsOld -->`, vendorsOld);
 
-            const vendorNames = vendors.data.map((vendor) => vendor.companyName);
+            // const vendorNames = vendors.data.map((vendor) => vendor.companyName);
 
-            console.log(`vendorNames -->`, vendorNames);
+            // console.log(`vendorNames -->`, vendorNames);
         } catch (error) {
             console.error(error);
         }
